454642506037e3f7266d4ed9c254173ac88bd161
EkaMoyak1 <moyekaLogin@gmail.com>
2025-03-02 19:35:47 +0900

    v1 star

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 import pygame
 import random
 import sys
+import time
+from config import *  # Импортируем настройки из config.py
 
 # Инициализация Pygame
 pygame.init()
 
 # Настройки экрана
-WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen.fill(bg_color)
 pygame.display.set_caption("Color Click Game")
 icon = pygame.image.load("img/icon.jpg")
 pygame.display.set_icon(icon)
 
-# Цвета
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
+star = pygame.image.load("img/star.png")
+red_star = pygame.image.load("img/red.png")
+green_star = pygame.image.load("img/green.png")
+blue_star = pygame.image.load("img/blue.png")
+yellow_star = pygame.image.load("img/yellow.png")
 
 # Шрифт
-font = pygame.font.Font(None, 36)
+font = pygame.font.Font(None, FONT_SIZE)
 
 # Класс для объектов
 class GameObject:
@@ -29,15 +29,17 @@ class GameObject:
         self.x = x
         self.y = y
         self.color = color
-        self.size = 50
+        self.img_name = f"{color}_star"
+        self.size = OBJECT_SIZE
         self.rect = pygame.Rect(self.x, self.y, self.size, self.size)
         self.clicked = False
+        self.spawn_time = time.time()  # Время появления объекта
 
     def draw(self):
         if self.clicked:
-            pygame.draw.rect(screen, self.color, self.rect)
+            screen.blit(eval(self.img_name), (self.x, self.y))
         else:
-            pygame.draw.rect(screen, WHITE, self.rect, 2)
+            screen.blit(star, (self.x, self.y))
 
     def check_click(self, pos):
         if self.rect.collidepoint(pos):
@@ -45,11 +47,15 @@ class GameObject:
             return self.color
         return None
 
+    def is_expired(self):
+        # Проверяем, прошло ли OBJECT_LIFETIME секунд с момента появления
+        return time.time() - self.spawn_time > OBJECT_LIFETIME
+
 # Функция для создания случайного объекта
 def create_random_object():
-    x = random.randint(0, WIDTH - 50)
-    y = random.randint(0, HEIGHT - 50)
-    color = random.choice([GREEN, RED, BLUE, YELLOW])
+    x = random.randint(0, WIDTH - OBJECT_SIZE)
+    y = random.randint(0, HEIGHT - OBJECT_SIZE)
+    color = random.choice(['green', 'red', 'blue', 'yellow'])
     return GameObject(x, y, color)
 
 # Основная функция игры
@@ -58,9 +64,10 @@ def main():
     score = 0
     objects = []
     game_over = False
+    last_spawn_time = time.time()  # Время последнего появления объекта
 
     while not game_over:
-        screen.fill(BLACK)
+        screen.fill(bg_color)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -69,19 +76,25 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 for obj in objects:
                     color = obj.check_click(event.pos)
+                    print(color)
                     if color:
-                        if color == GREEN:
+                        if color == 'green':
                             score += 5
-                        elif color == RED:
+                        elif color == 'red':
                             score -= 5
-                        elif color == BLUE:
+                        elif color == 'blue':
                             score += 3
-                        elif color == YELLOW:
+                        elif color == 'yellow':
                             score += 1
 
-        # Создаем новые объекты
-        if len(objects) < 5:
+        # Появление объектов каждую секунду
+        current_time = time.time()
+        if current_time - last_spawn_time >= SPAWN_INTERVAL:
             objects.append(create_random_object())
+            last_spawn_time = current_time
+
+        # Удаляем объекты, которые "просрочены"
+        objects = [obj for obj in objects if not obj.is_expired()]
 
         # Рисуем объекты
         for obj in objects:
@@ -92,11 +105,11 @@ def main():
         screen.blit(score_text, (10, 10))
 
         # Проверяем условия победы и проигрыша
-        if score >= 20:
+        if score >= WIN_SCORE:
             win_text = font.render("You Win!", True, GREEN)
             screen.blit(win_text, (WIDTH // 2 - 50, HEIGHT // 2))
             game_over = True
-        elif score < 0:
+        elif score <= LOSE_SCORE:
             lose_text = font.render("You Lose!", True, RED)
             screen.blit(lose_text, (WIDTH // 2 - 50, HEIGHT // 2))
             game_over = True
